a0c9803d53dbb9ad36e7daa38114cfdcf0b1cbae
Jakub Veverka <veverka.kuba@gmail.com>
2015-07-07 14:27:08 +0200

    Restart bamboo under supervisord 10000 times before failure

--- builder/run.sh
+++ builder/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+STATSD_IP=$IP
+STATSD_PORT=${STATSD_PORT-8125}
 confd -onetime -backend etcd -node=`cat /etc/ip`:4001 -prefix=$ETCD_BASEPATH
 haproxy -f /etc/haproxy/haproxy.cfg -p /var/run/haproxy.pid
 /usr/bin/supervisord
